e9e93de01f67f9ce6f416638299b031af20ecda6
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-10-24 20:28:14 +0200

    Changed everything to work better with js

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-// const button = document.querySelector(".button");
-// const radio = document.querySelector(".radio__input");
+let rating__btn = document.querySelectorAll(".rating-btn");
+let btn = document.getElementById("btn");
+let page__unsubmitted = document.querySelector(".page__unsubmitted"); // evalutation div //
+let page__submitted = document.querySelector(".page__submitted"); // thank you div //
+let span = document.getElementById("rating"); // empty span where the user selected rating will be displayed//
 
-// if (radio.checked === true) {
-//     button.addEventListener("click", () => {
-//         document.querySelector(".hidden").classList.remove("hidden");
-//         document.querySelector(".unsubmitted").classList.add("hidden");
-//     });
-// } else {
-// }
+// Looping through all the number divs//
+for (let i = 0; i < rating__btn.length; i++) {
+    // Listening for a click event on each number div //
+    rating__btn[i].addEventListener("click", () => {
+        // getting the clicked number value property from 1 to 5//
+        let rating = rating__btn[i].innerHTML;
 
-const radio = document.getElementById("radio");
-const button = document.querySelector(".button");
+        // looping through the numbers to check for a previously selected rating and removing if such. //
+        // Necessary in order to have only one rating selected at a time //
+        for (let i = 0; i < rating__btn.length; i++) {
+            rating__btn[i].classList.contains("selected");
+            rating__btn[i].classList.remove("selected");
+        }
 
-button.addEventListener("click", () => {
-    const hiddenElement = document.querySelector(".hidden");
-    const unsubmittedElement = document.querySelector(".unsubmitted");
+        // add the selected class which highlights the selected rating //
+        rating__btn[i].classList.add("selected");
 
-    hiddenElement.classList.remove("hidden");
-    unsubmittedElement.classList.add("hidden");
-});
+        // listening for a click on the btn in order to hide the current div and display the Thank You div with the chosen rating in the span //
+        btn.addEventListener("click", () => {
+            page__unsubmitted.style.display = "none";
+            page__submitted.style.display = "block";
+            span.innerHTML = rating;
+        });
+    });
+}
