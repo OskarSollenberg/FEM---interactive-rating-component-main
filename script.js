2384376a758c392cf6d2da25782faa5d6eba1440
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-11-06 14:00:41 +0100

    js stuff

---
+++ script.js
@@ -0,0 +1,16 @@
+// const ratingBtn = document.querySelectorAll(".rating-btn");
+
+// addClass = (element, className) => {
+//     element.classList.add(className);
+// };
+
+// ratingBtn.forEach((btn) => {
+//     btn.addEventListener("click", () => {
+//         addClass(ratingBtn, "selected");
+//     });
+// });
+
+// for (let i = 0; i < ratingBtn.length; i++) {
+
+//  ratingBtn.forEach((btn) => {
+//         addClass(ratingBtn, "selected");
